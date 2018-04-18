51c9d393089f8cdaa898d8b9fa386cd5860a66ae
Poroshochek <mr.soluk@gmail.com>
2018-04-18 09:41:50 +0300

    Create vendor

---
+++ config/init.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/webshop/core');
+define("LIBS", ROOT . '/vendor/webshop/core/libs');
+define("CACHE", ROOT . '/vendor/webshop/core/libs');
