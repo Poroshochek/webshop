b4e2be0d9f28f81a6415810f8fde65e220b25bdd
Poroshochek <mr.soluk@gmail.com>
2018-04-26 16:44:40 +0300

    show brands

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,8 +11,8 @@ class MainController extends AppController
 
     public function indexAction()
     {
-
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Main page', 'Descript...', 'Keywords');
-
+        $this->set(compact('brands'));
     }
 }
