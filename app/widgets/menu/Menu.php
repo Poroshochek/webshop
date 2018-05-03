db310abec755117be420aaed1ed75f5746048cf1
Poroshochek <mr.soluk@gmail.com>
2018-05-03 17:31:37 +0300

    creating menu widget

---
+++ app/widgets/menu/Menu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ESoluk
+ * Date: 03.05.2018
+ * Time: 12:00
+ */
+
+namespace app\widgets\menu;
+
+
+class Menu
+{
+
+}
---
+++ public/menu/menu.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ESoluk
+ * Date: 03.05.2018
+ * Time: 12:08
+ */
